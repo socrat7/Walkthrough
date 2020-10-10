e70c8376d10f6fc9c0ac61e7b80f8998ff46f552
49157 <49157@Desktop>
2020-10-10 22:10:49 +0200

    Walkthrough - Step 6 - Modules

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,16 +1,14 @@
 sap.ui.define([
 
-	"sap/ui/core/mvc/Controller"
+	"sap/ui/core/mvc/Controller",
+	"sap/m/MessageToast"
 
-], function(Controller) {
+], function(Controller, MessageToast) {
 
 	return Controller.extend("sample1.controller.App", {
 
 		onShowHello: function() {
-			// show a native JavaScript alert
-			/* eslint-disable no-alert */
-			alert("Hello World");
-			/* eslint-enable no-alert */
+			MessageToast.show("Hello World");
 		}
 	});
 });
