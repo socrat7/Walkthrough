258e9457ea02b220a21bd37828c9360d3fb94555
49157 <49157@Desktop>
2020-10-10 20:33:08 +0200

    Walkthrough - Step 4 - XML Views

--- webapp/index.js
+++ webapp/index.js
@@ -1,16 +1,22 @@
 sap.ui.define([
-	"sap/m/Text"
 
-], function (Text) {
+	"sap/ui/core/mvc/XMLView"
+
+], function (XMLView) {
+	
 	"use strict";
 
-	/* eslint-disable no-alert */
-	alert("UI5 is ready");
-	/* eslint-enable no-alert */
+	XMLView.create({
+		
+		viewName: "sample1.view.App"
+		
+	}).then(function(oView){
+		
+		oView.placeAt("content");
+		
+	});
+    
 	
 	
-	new Text({
-		text: "Hello World"
-	}).placeAt("content");
 	
 });
